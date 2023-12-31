e8265b32bc6e22e5a3a2fcdd4b144eb1c3db9126
ibhagyesh <bhagyeshjahangirpuriya@gmail.com>
2023-12-31 15:51:51 +0530

    fixed auth provider confusion

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,33 @@ function App() {
   // auth check logic
   const { auth } = useAuth();
 
+  // the auth logic when page refresh
+  // for cookies
+  // useEffect(() => {
+  //   if (Cookies.get("x-app-token") && !auth) {
+  //     // extract user data from token in cookie and set the auth context
+  //     const decoded = jwtDecode(Cookies.get("x-app-token"));
+  //     setAuth({ id: decoded.user_id });
+  //   }
+  // }, [auth]);
+
+  // for token
+  useEffect(() => {
+    if (
+      localStorage.getItem("x-app-token") &&
+      localStorage.getItem("x-app-user-id") &&
+      localStorage.getItem("x-app-user-name") &&
+      !auth.token
+    ) {
+      // extract user data from token in cookie and set the auth context
+      setAuth({
+        id: localStorage.getItem("x-app-user-id"),
+        name: localStorage.getItem("x-app-user-name"),
+        token: localStorage.getItem("x-app-token"),
+      });
+    }
+  }, [auth]);
+
   const routesData = useRoutes(
     auth?.token // when user logs in
       ? authenticatedRoutes
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -6,33 +6,6 @@ const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
 
-  // the auth logic when page refresh
-  // for cookies
-  // useEffect(() => {
-  //   if (Cookies.get("x-app-token") && !auth) {
-  //     // extract user data from token in cookie and set the auth context
-  //     const decoded = jwtDecode(Cookies.get("x-app-token"));
-  //     setAuth({ id: decoded.user_id });
-  //   }
-  // }, [auth]);
-
-  // for token
-  useEffect(() => {
-    if (
-      localStorage.getItem("x-app-token") &&
-      localStorage.getItem("x-app-user-id") &&
-      localStorage.getItem("x-app-user-name") &&
-      !auth.token
-    ) {
-      // extract user data from token in cookie and set the auth context
-      setAuth({
-        id: localStorage.getItem("x-app-user-id"),
-        name: localStorage.getItem("x-app-user-name"),
-        token: localStorage.getItem("x-app-token"),
-      });
-    }
-  }, [auth]);
-
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
